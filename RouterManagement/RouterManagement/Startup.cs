77b56acdea09d9aa2a1e86188b0e93b1b73c361f
Paweł Maciejewski <paulomac1000@gmail.com>
2017-03-25 20:45:46 +0100

    #master raw version of project with added connectionstring to free database on amazon and apply migration to it

---
+++ RouterManagement/RouterManagement/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(RouterManagement.Startup))]
+namespace RouterManagement
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
