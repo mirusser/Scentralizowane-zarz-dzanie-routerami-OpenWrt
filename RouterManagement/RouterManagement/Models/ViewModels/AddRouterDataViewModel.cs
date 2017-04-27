db9be79c2ed4144ba184aecfae8b9be38a1e6f95
Pawel Maciejewski <pmaciejewski@openskydata.com>
2017-04-27 12:42:00 +0200

    #development-Pawel updated regex for checking router adress or ulr and add glyphicon  icons for showing router activity

--- RouterManagement/RouterManagement/Models/ViewModels/AddRouterDataViewModel.cs
+++ RouterManagement/RouterManagement/Models/ViewModels/AddRouterDataViewModel.cs
@@ -7,7 +7,7 @@ namespace RouterManagement.Models.ViewModels
     {
         [StringNotNullNotEmpty]
         public string Name { get; set; }
-        [RegularExpression(@"^([a-zA-Z0-9]|[a-zA-Z0-9][a-zA-Z0-9\-]{0,61}[a-zA-Z0-9])(\.([a-zA-Z0-9]|[a-zA-Z0-9][a-zA-Z0-9\-]{0,61}[a-zA-Z0-9]))*$")]
+        [RegularExpression(@"^((((([a-zA-Z]+|[a-zA-Z0-9]*)\.)+)(([a-zA-Z]+[a-zA-Z0-9]*)+))|((\d|[1-9]\d|1\d\d|2([0-4]\d|5[0-5]))\.(\d|[1-9]\d|1\d\d|2([0-4]\d|5[0-5]))\.(\d|[1-9]\d|1\d\d|2([0-4]\d|5[0-5]))\.(\d|[1-9]\d|1\d\d|2([0-4]\d|5[0-5])))|(localhost))$")]
         public string RouterIp { get; set; }
         public int? Port { get; set; }
         [StringNotNullNotEmpty]
--- RouterManagement/RouterManagement/Models/ViewModels/ModifyRouterDataViewModel.cs
+++ RouterManagement/RouterManagement/Models/ViewModels/ModifyRouterDataViewModel.cs
@@ -9,7 +9,7 @@ namespace RouterManagement.Models.ViewModels
         public string Name { get; set; }
         [StringNotNullNotEmpty]
         public string OldName { get; set; }
-        [RegularExpression(@"^([a-zA-Z0-9]|[a-zA-Z0-9][a-zA-Z0-9\-]{0,61}[a-zA-Z0-9])(\.([a-zA-Z0-9]|[a-zA-Z0-9][a-zA-Z0-9\-]{0,61}[a-zA-Z0-9]))*$")]
+        [RegularExpression(@"^((((([a-zA-Z]+|[a-zA-Z0-9]*)\.)+)(([a-zA-Z]+[a-zA-Z0-9]*)+))|((\d|[1-9]\d|1\d\d|2([0-4]\d|5[0-5]))\.(\d|[1-9]\d|1\d\d|2([0-4]\d|5[0-5]))\.(\d|[1-9]\d|1\d\d|2([0-4]\d|5[0-5]))\.(\d|[1-9]\d|1\d\d|2([0-4]\d|5[0-5])))|(localhost))$")]
         public string RouterIp { get; set; }
         public int? Port { get; set; }
         [StringNotNullNotEmpty]
